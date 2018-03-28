e8ed8b3e88d95e1327ca5f5b4c6fe535b2d0dbbf
crizen <crizen@CZ-hd-N1>
2018-03-28 15:58:07 +0900

    Test

--- spring-data-rest-example/src/main/java/com/khjeon/repository/ArticleRepository.java
+++ spring-data-rest-example/src/main/java/com/khjeon/repository/ArticleRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.khjeon.domain.Article;
 
+//왜안되
+
 public interface ArticleRepository extends JpaRepository<Article, Integer> {}
